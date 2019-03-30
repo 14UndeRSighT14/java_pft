fbd28166192e94668ec6620b5195dc4ca330710e
14UndeRSighT14 <48184098+14UndeRSighT14@users.noreply.github.com>
2019-03-30 14:42:39 +0500

    Добавление класса и метода для редактирования группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class ContactCreationTests extends TestBase {
 
   @Test
-  public void testContactCreationTests() throws Exception {
+  public void testContactCreation() throws Exception {
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactCreation();
     app.getContactHelper().fillPartContactForm(new ContactData("Name1", "Name2", "Name3", "Super", "Title", "Super", "Address", "8(3472)111111", "89871111111", "89872222222", "test1@mail.ru", "test2@mail.ru", "test3@mail.ru", "www.yandex.ru", "14", "January", "1925", "Address2", "89874444444", "Notes"));
